64d1e736be236bb25388787b55f8f9124bcd8196
mariajosemeneses <majom875@gmail.com>
2021-07-22 22:14:06 -0500

    union

--- freelancer_inicio/freelancer_inicio/javaScript/popup.js
+++
@@ -1,9 +0,0 @@
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
